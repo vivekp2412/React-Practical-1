7af01717774ddac90f582f176d9edb5391659d55
vivekp2412 <vivekp2412@gmail.com>
2023-04-18 16:56:19 +0530

    React App Created with Webpack

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "App.css";
+import thumb from "./React-icon.svg.png"
+const App = ()=>(
+    <div>
+        <h1 className="blue">Hello</h1>
+        <img src={thumb} width="50px" height="50px"></img>
+    </div>
+)
+export default App;
